497c0523bcf7db7edb8af60c80d289c768dbbdc9
Bozhidar Dimov <bojko958@abv.bg>
2017-06-14 21:52:41 +0300

    determined for which objects is called the copy-constructor - for the atomic automatas

--- DoubleTapedAutomata/RPNParser.h
+++ DoubleTapedAutomata/RPNParser.h
@@ -67,24 +67,32 @@ public:
 
 		for (int i = 0; i < input.size(); i++) {
 			if (input[i] == '*') {
+				std::cout << "Start of '*' operation\n";
 				//check for correctness of stack
-				operands[operands.size() - 1].Star();	
+				operands[operands.size() - 1].Star();
+				std::cout << "";
 			}
 			else if (input[i] == '|') {
+				std::cout << "Start of '|' operation\n";
 				//check for correctness of stack
 				operands[operands.size() - 2].Union(operands[operands.size() - 1]);
 				operands.pop_back();
+				std::cout << "End of '|' operation\n";
 			}
 			else if (input[i] == '.') {
+				std::cout << "Start of '.' operation\n";
 				//check for correctness of stack
 				operands[operands.size() - 2].Concatenate(operands[operands.size() - 1]);
 				operands.pop_back();
+				std::cout << "End of '.' operation\n";
 			}
 			else {
 				monoid_checker.register_symbol(input[i]);
 				if (monoid_checker.success()) {
+					std::cout << "Start of monoid creation\n";
 					operands.push_back(monoid_checker.factory());
 					monoid_checker.reset();
+					std::cout << "End of monoid creation\n";
 				}
 			}
 		}
--- DoubleTapedAutomata/main.cpp
+++ DoubleTapedAutomata/main.cpp
@@ -3,7 +3,7 @@
 
 ///Automata(state_descriptor(state, is_starting, is_final) transitions(source, m, dest)
 int main() {
-	Automata A1(
+	/*Automata A1(
 		{
 			{ 1, true, false },
 			{ 2, false, true }
@@ -13,11 +13,13 @@ int main() {
 		});
 	auto copy_temp = A1;
 	A1.Concatenate(copy_temp).Union(copy_temp).Star();
-
+*/
 	std::string automata1_input = "(a,1)(a,1).(a,1)|*";
 	RPNParser_vector_operands parser;
 	//logic is OK
 	//too many calls of copy constructor
+	//reworked the functions to work with references
+	//copy-constructors are called only for the monoid automatas(very small ones)
 	auto result = parser.parse(automata1_input);
 	return 0;
 }
